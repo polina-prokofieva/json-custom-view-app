e32523655686cdcf7a3606fd74a22738f8f91d57
Polina Prokofieva <polina.prokofieva@gmail.com>
2023-03-07 11:07:31 +0200

    Updated lib version

--- src/pages/FullViewPage/GeneratedView/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'json-custom-view' {
-  export const generate: (
-    json: string | object,
-    mainNode: HTMLElement,
-    settings: object
-  ) => void;
-
-  export const transform: (json: object, settings: object) => object;
-}
